049a4dc60db9a30c01c42240f3c19546daad3027
Kevin <kevin.haya@yahoo.com>
2022-09-10 23:39:15 +0800

    Refractored event listeners

--- src/index.js
+++ src/index.js
@@ -1,34 +1,52 @@
-import {UI} from './modules/UI'
-
-
+import {notesUI} from './modules/UI'
 
 const notes = document.getElementById("notes");
 const today = document.getElementById("today");
 const week = document.getElementById("week");
 const projects = document.getElementById("projects");
 
-notes.addEventListener("click", show)
-today.addEventListener("click", show)
-week.addEventListener("click", show)
-projects.addEventListener("click", show)
-
-
 const nav = [notes,today,week,projects]
 
-function active(element){
+document.body.addEventListener("click", (e) => {
+    let parentElement = e.target.parentElement.tagName.toLowerCase()
 
+    switch(parentElement) {
+        case "nav":
+            showTab(e)
+            break;
+        case "p":
+            notesEvents(e)
+            break;
+        default:
+            console.log("alert")
+      }
+});
+
+function showTab(e){
+    toggleActive(e)
 }
 
-
-function show(e){
-    e.target.classList.add("active")
-    for(let i=0;i<nav.length;i++){
-        if(e.target != nav[i]){
-            nav[i].classList.remove("active")
-        }
+function notesEvents(e){
+    let element = e.target.tagName.toLowerCase();
+    switch(element){
+        case "i":
+            console.log(e.target)
+            e.target.className = "";
+            e.target.classList.add("fa-regular","fa-circle-check");
+            break;
+        case "span":
+            break;
+        default:
+            console.log("alert2")
     }
 }
 
+function toggleActive(e){
+    e.target.classList.add("active")
+    nav.forEach(element => {if(e.target != element){
+        element.classList.remove("active")
+    }})
+}
 
 
-UI()
+// UI()
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -2,23 +2,34 @@ import {notesContent} from './notes.js'
 
 const section = document.getElementsByTagName("section")[0];
 
-// const notes = document.getElementById("notes");
-// const today = document.getElementById("today");
-// const week = document.getElementById("week");
-// const projects = document.getElementById("rojects");
 
-// notes.addEventListener("click", showNotes)
-
-function UI(){
+function notesUI(){
     section.className = "";
     section.classList.add("notes")
     section.innerHTML = notesContent;
+      // const p = document.createElement("p");
+    // p.classList.add("note-detail")
+
 }
 
-// function showNotes(){
 
-// }
+const defaultUI = (() => {
+    const notes = document.getElementById("notes");
+    notes.classList.add("active");
+    section.className = "";
+    section.classList.add("notes")
+    section.innerHTML = notesContent;
+})();
 
+export {notesUI}
 
+// let notesContent = `
+//     <p class="note-detail">
+//         <button><i class="fa-regular fa-circle"></button></i>
+//         <span>  Details</span>
+//         <input type="date" class="date">
+//         <button class="delete-note"><i class="fa-solid fa-trash-can"></i></button>
+//     </p>
 
-export {UI}
+//     <button id="add-note"><i class="fa-solid fa-plus"></i> Add Note</button>
+// `
